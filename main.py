c8c5dd0aba01a2d076454228e1a61a793f711639
Cisco89 <franciscoadriangarcia1@gmail.com>
2019-06-17 00:48:43 -0600

    Added forms to hello-flask

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+form = """
+<!doctype html>
+<html>
+    <body>
+        <form action="/hello" method="post">
+            <label for="first_name">First Name</label>
+            <input type="text" name="first_name" />
+            <input type="submit" />
+        </form>
+    </body>
+</html>
+"""
+
 @app.route("/")
 def index():
-    return "Hello World"
+    return form
+
+@app.route("/hello", methods=['POST'])
+def hello():
+    # first_name = request.args.get('first_name')   # for GET request
+    first_name = request.form['first_name']         # for a POST Request
+    return '<h1>Hello, ' + first_name + '</h1>'
+
+@app.route("/narnia/")
+def narnia():
+    return "Welcome to Narnia"
+
+@app.route("/mexico/")
+def mexico():
+    return "Hola Mundo"
+
 
 app.run()
